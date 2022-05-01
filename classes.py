e89e2a2829423bc3056c8466eec30a7b285f572c
nchubbsh <nchubbsh@ualberta.ca>
2022-05-01 17:36:09 -0600

    update

--- classes.py
+++ classes.py
@@ -2,7 +2,24 @@ import pygame
 from pygame.locals import *
 
 
-class Rect:
+class Guess:
 
     def __init__(self, screen, left, top, width, height):
+
+        self.screen = screen
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
         self.rect = pygame.draw.rect(screen, (127,127,127), (left,top,width,height), 2)
+        self.font = pygame.font.SysFont("Roboto", 80)
+    
+    def add_guess(self, letter):
+
+        w, h = self.font.size(letter)
+
+        xoff = (self.width - w) // 2
+        yoff = (self.height - h) // 2
+
+        self.screen.blit(self.font.render(letter, True, (255,255,255)), (self.left + xoff, self.top + yoff+4))
+        pygame.display.update()
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import classes as cla
 
 def main():
     pygame.init()
+    pygame.display.set_caption("word-guess")
     screen = pygame.display.set_mode((420,840))
 
     guess_space = []
@@ -13,9 +14,22 @@ def main():
     for j in range(1, 7):
         row = []
         for i in range (1, 6):
-            temp = cla.Rect(screen, (60*i), (60*j), 60, 60)
+            temp = cla.Guess(screen, (60*i), (60*j), 60, 60)
             row.append(temp)
         guess_space.append(row)
+
+    alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    i = 0
+    j = 0
+    for letter in alp:
+        guess_space[i][j].add_guess(letter)
+        j += 1
+        if (j == 5):
+            i += 1
+            j = 0
+
+        if(i == 6):
+            break
 
     driver_gate = True
 
