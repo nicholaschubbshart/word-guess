26798aeb9ebbe213c102775e29f3f804ce9d1523
nchubbsh <nchubbsh@ualberta.ca>
2022-05-01 18:25:58 -0600

    added keyboard

--- classes.py
+++ classes.py
@@ -23,3 +23,30 @@ class Guess:
 
         self.screen.blit(self.font.render(letter, True, (255,255,255)), (self.left + xoff, self.top + yoff+4))
         pygame.display.update()
+
+
+class Key:
+
+    def __init__(self, screen, left, top, width, height, key):
+
+        self.screen = screen
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+        self.key = key
+        self.rect = pygame.draw.rect(screen, (127,127,127), (left,top,width,height), 2)
+        self.font = pygame.font.SysFont("Roboto", 30)
+    
+    def add_key(self):
+
+        w, h = self.font.size(self.key)
+
+        xoff = (self.width - w) // 2
+        yoff = (self.height - h) // 2
+
+        self.screen.blit(self.font.render(self.key, True, (255,255,255)), (self.left + xoff, self.top + yoff+2))
+        pygame.display.update()
+
+    def get_key(self):
+        return self.key
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
 from pygame.locals import *
 
+import sys
+
 import classes as cla
+
+def get_pressed(key_board, cords):
+
+    for row in key_board:
+        for col in row:
+            left, top = col[1]
+            
+            if ((left <= cords[0] and cords[0] <= (left + 30)) and (top <= cords[1] and cords[1] <= (top + 30))):
+                return col[0].get_key()
 
 
 def main():
     pygame.init()
     pygame.display.set_caption("word-guess")
-    screen = pygame.display.set_mode((420,840))
+    screen = pygame.display.set_mode((420,700))
 
     guess_space = []
+    key_board = []
 
+    # create guess space
     for j in range(1, 7):
         row = []
         for i in range (1, 6):
@@ -18,29 +31,55 @@ def main():
             row.append(temp)
         guess_space.append(row)
 
-    alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    i = 0
-    j = 0
-    for letter in alp:
-        guess_space[i][j].add_guess(letter)
-        j += 1
-        if (j == 5):
-            i += 1
-            j = 0
+    # create keyboard
+    key_top = (60*8)
+    key_left = 60
+    layout = [10, 9, 7]
+    keys = ["QWERTYUIOP","ASDFGHJKL","ZXCVBNM"]
 
-        if(i == 6):
-            break
+    for j in range(0, 3):
 
-    driver_gate = True
+        if (j == 2):
+            key_left += 45
+        else:
+            key_left += (15*j)
+
+        row = []
+        for i in range (0, layout[j]):
+            temp = cla.Key(screen, key_left, key_top, 30, 30, keys[j][i])
+            temp.add_key()
+            row.append([temp, [key_left, key_top]])
+            key_left += 30
+
+        key_left = 60
+        key_top += 30
+        key_board.append(row)
+    
+
+    # # TO BE REMOVED
+    # alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # i = 0 # row number
+    # j = 0 # col number
+    # for letter in alp:
+    #     guess_space[i][j].add_guess(letter)
+    #     j += 1
+    #     if (j == 5):
+    #         i += 1
+    #         j = 0
+
+    #     if(i == 6):
+    #         break
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                driver_gate = False
+                sys.exit()
 
-        if(driver_gate == False):
-            break
+            if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed():
+                pos = pygame.mouse.get_pos()
+                pressed = get_pressed(key_board, pos)
+                print(pressed)
 
         pygame.display.update()
 
